a982b6def685b930dfbcfbb45d8f94a96b7bf06c
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2026-09-10 12:41:44 +0100

    Update Hyperlinks.js

--- Assets/Hyperlinks.js
+++ Assets/Hyperlinks.js
@@ -1,12 +1,12 @@
 // ***** IMAGES & ASSETS
 /** Image of Discord's Button Colours (last updated 2022) */
-export const IMAGE_BUTTON_COLORS = "https://us-east-1.tixte.net/uploads/twilite.is-from.space/discordButtonColours.png";
+export const IMAGE_BUTTON_COLORS = "https://raw.githubusercontent.com/TwilightZebby/TwiLite/refs/heads/main/TotallyNotACDN/Misc/discordButtonColours.png";
 
 /** Image of TwiLite's Context Commands (for Role Menu Commands) */
-export const IMAGE_TWILITE_ROLEMENU_CONTEXT_COMMANDS = "https://us-east-1.tixte.net/uploads/twilite.is-from.space/TwiLiteRoleMenuCtxCmds.png";
+export const IMAGE_TWILITE_ROLEMENU_CONTEXT_COMMANDS = "https://raw.githubusercontent.com/TwilightZebby/TwiLite/refs/heads/main/TotallyNotACDN/Commands/roleMenuContextCommands.png";
 
 /** PNG Image of Cellbars for the Jail Action Command */
-export const IMAGE_JAIL_CELLBARS = "https://us-east-1.tixte.net/uploads/twilite.is-from.space/action-jail-cellbars.png"
+export const IMAGE_JAIL_CELLBARS = "https://raw.githubusercontent.com/TwilightZebby/TwiLite/refs/heads/main/TotallyNotACDN/Misc/actionJailCellbars.png"
 
 
 // ***** URIs
